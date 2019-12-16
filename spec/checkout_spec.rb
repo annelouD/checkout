8c3ef6e150cfd8bf5d6b335690d4e1de28cdcc34
Anne-Louise Darko <ms.darko@live.co.uk>
2019-12-16 11:41:51 +0000

    test2 scan ability fail

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -4,4 +4,9 @@ describe Checkout do
   it 'expects a class which creates instances' do
     expect(Checkout.new).to be_a(Checkout)
   end
+
+  it 'allows user to scan items' do
+    expect(Checkout.new).to respond_to(:scan).with(1).argument
+  end
+
 end
